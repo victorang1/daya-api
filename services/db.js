9eb8da92728a9d637651660214466dc7de6440ab
victor <victor@binus.edu>
2021-05-28 23:29:08 +0700

    fix yaml

--- services/db.js
+++ services/db.js
@@ -1,68 +1,17 @@
 const mysql = require('mysql2/promise');
 
-const winston = require('winston');
-const { LoggingWinston } = require('@google-cloud/logging-winston');
-const loggingWinston = new LoggingWinston();
-const logger = winston.createLogger({
-  level: 'info',
-  transports: [new winston.transports.Console(), loggingWinston],
-});
-
-const createTcpPool = async config => {
-  const dbSocketAddr = process.env.DB_HOST.split(':');
-
-  return await mysql.createPool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: dbSocketAddr[0],
-    port: dbSocketAddr[1],
-    ...config,
-  });
-};
-
-const createUnixSocketPool = async config => {
-  const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
-
-  return await mysql.createPool({
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    socketPath: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
-    ...config,
-  });
-};
-
-const createPool = async () => {
-  const config = {
-    connectionLimit: 5,
-    connectTimeout: 10000,
-    acquireTimeout: 10000,
-    waitForConnections: true,
-    queueLimit: 0,
-  };
-  if (process.env.DB_HOST) {
-    return await createTcpPool(config);
-  } else {
-    return await createUnixSocketPool(config);
-  }
-};
-
-const createPoolAndEnsureSchema = async () =>
-  await createPool()
-    .then(async pool => {
-      return pool;
-    })
-    .catch(err => {
-      logger.error(err);
-      throw err;
-    });
-
-let pool;
+var config = {
+    user: process.env.SQL_USER,
+    database: process.env.SQL_DATABASE,
+    password: process.env.SQL_PASSWORD
+}
 
 async function query(sql, params) {
-  pool = pool || (await createPoolAndEnsureSchema());
-  const results = await pool.query(sql, params);
+  if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+    config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+  }
+  const connection = await mysql.createConnection(config);
+  const [results, ] = await connection.execute(sql, params);
 
   return results;
 }
